bb95a236a0a9bf2571b2a65a63339c287d5668a2
ramzi mosrati <mosrati.ramzi@gmail.com>
2023-09-26 10:01:28 +0200

    read input from file and execute jar

--- src/main/java/com/rmosrati/RoverApp.java
+++ src/main/java/com/rmosrati/RoverApp.java
@@ -1,19 +1,33 @@
 package com.rmosrati;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class RoverApp {
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // args [0] is the absolute path of input.txt
+        // example C:\Users\Desktop\input.txt
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(args[0]));
+            String plateauSize = reader.readLine();
+            String line = reader.readLine();
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+            while (line != null) {
+                String[] position = line.split(" ");
+                Rover rover = new Rover(Integer.parseInt(position[0]), Integer.parseInt(position[1]), position[2]);
+                // read next line
+                String newPosition = rover.navigate(reader.readLine());
+                line = reader.readLine();
+                System.out.println(newPosition);
+            }
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
